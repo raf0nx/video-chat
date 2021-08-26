be77c269abbc47f130f811536d7322321c124af2
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-08-27 00:00:50 +0200

    minor: Make vue and api communicate each other

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,21 @@
 <template>
 	<div class="dashboard">
 		<h1>Dashboard</h1>
+		<button @click="sendGetRequest()">Click me!</button>
 	</div>
 </template>
 
 <script>
-	export default {};
+	import axios from "axios";
+
+	export default {
+		methods: {
+			async sendGetRequest() {
+				const message = await axios.get("http://localhost:3000/api");
+				console.log(message.data.message);
+			},
+		},
+	};
 </script>
 
 <style scoped>
