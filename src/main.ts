5f46589cc9770377a87ab326c11966a5aecd0b6f
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-08-29 15:27:03 +0200

    minor: Add vue-socket-io configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import VueSocketIO from 'vue-socket.io';
+import vuetify from './plugins/vuetify';
+import store from './store/store';
 
 Vue.config.productionTip = false;
 
+Vue.use(
+	new VueSocketIO({
+		debug: true,
+		connection: 'http://localhost:8080',
+		vuex: {
+			store,
+			actionPrefix: 'SOCKET_',
+			mutationPrefix: 'SOCKET_',
+		},
+	})
+);
+
 new Vue({
+	store,
 	vuetify,
 	render: h => h(App),
-}).$mount("#app");
+}).$mount('#app');
